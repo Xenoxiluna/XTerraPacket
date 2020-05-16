b90e04bb22567d8a25e5df633b98317626118ac0
Xenoxiluna <xenoxiluna@live.com>
2020-05-15 19:50:24 -0500

    Hopefully fixed PlayerUpdate

--- Sources/XTerraPacket/Types/PacketPlayerUpdate.swift
+++ Sources/XTerraPacket/Types/PacketPlayerUpdate.swift
@@ -14,13 +14,13 @@ public struct PacketPlayerUpdate: TerrariaPacket{
     public var packetType: TerrariaPacketType = .PlayerUpdate
     public var payload: [UInt8] = []
     public var playerId: UInt8 = 0
-    public var control: UInt8 = 0
-    public var pulley: UInt8 = 0
+    public var control: UInt8 = 0 // Bit Flags - ControlUp = 1, ControlDown = 2, ControlLeft = 4, ControlRight = 8, ControlJump = 16, ControlUseItem = 32, Direction = 64|
+    public var pulley: UInt8 = 0 // Bit Flags -  0 = None, 1 = Direction, 2 = Direction, 4 = Update Velocity, 8 = Vortex Stealth Active, 16 = Gravity Direction, 32 = Shield Raised|
     public var item: UInt8 = 0
-    public var x: Float32 = 0
-    public var y: Float32 = 0
-    public var velx: Float32 = 0
-    public var vely: Float32 = 0
+    public var positionX: Float32 = 0
+    public var positionY: Float32 = 0
+    public var velocityX: Float32 = 0
+    public var velocityY: Float32 = 0
     
     public init(){}
     
@@ -34,10 +34,14 @@ public struct PacketPlayerUpdate: TerrariaPacket{
         self.control = try reader.readUInt8()
         self.pulley = try reader.readUInt8()
         self.item = try reader.readUInt8()
-        self.x = try reader.readFloat32()
-        self.y = try reader.readFloat32()
-        self.velx = try reader.readFloat32()
-        self.vely = try reader.readFloat32()
+        self.positionX = try reader.readFloat32()
+        self.positionY = try reader.readFloat32()
+        
+        // Only read if Update Velocity is set
+        if (self.pulley & (1 << 2)) != 0{
+            self.velocityX = try reader.readFloat32()
+            self.velocityY = try reader.readFloat32()
+        }
     }
     mutating public func encodePayload() throws{
         self.resetPayload()
@@ -46,10 +50,13 @@ public struct PacketPlayerUpdate: TerrariaPacket{
         try writer.writeUInt8(control)
         try writer.writeUInt8(pulley)
         try writer.writeUInt8(item)
-        try writer.writeFloat32(x)
-        try writer.writeFloat32(y)
-        try writer.writeFloat32(velx)
-        try writer.writeFloat32(vely)
+        try writer.writeFloat32(positionX)
+        try writer.writeFloat32(positionY)
+        
+        if (self.pulley & (1 << 2)) != 0{
+            try writer.writeFloat32(velocityX)
+            try writer.writeFloat32(velocityY)
+        }
         payload.append(contentsOf: writer.data)
     }
 }
