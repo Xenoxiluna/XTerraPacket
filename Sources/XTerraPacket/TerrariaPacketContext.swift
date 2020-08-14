4fc79d12128bcd050f1f9c0f1365e06174436dd9
Xenoxiluna <xenoxiluna@live.com>
2020-08-14 12:05:12 -0500

    Create TerrariaPacketContext.swift

---
+++ Sources/XTerraPacket/TerrariaPacketContext.swift
@@ -0,0 +1,14 @@
+//
+//  TerrariaPacketContext.swift
+//  
+//
+//  Created by Quentin Berry on 8/14/20.
+//
+
+import Foundation
+
+/// This is an enum for setting a packets directional context
+public enum TerrariaPacketContext{
+    case ServerToClient
+    case ClientToServer
+}
